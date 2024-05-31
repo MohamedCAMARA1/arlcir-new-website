da6016ebe498bb4e40dad7cdde2e4f73ba465de8
Mohamed CAMARA <mohamed.camara@lilo.org>
2024-05-31 01:27:55 +0000

    ajout version node dans le package.json

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import DonationONG from "./components/DonationONG";
-import ReturnPage from "./components/ReturnPage";
+import DonationONG from "./Component/DonationONG";
+import ReturnPage from "./Component/ReturnPage";
 
 const App = () => {
   return (
